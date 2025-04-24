931ef307c100c7e57d110e748ec1e13f1257d542
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-04-24 22:23:23 +0530

    add login page

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -30,7 +30,7 @@ const Footer = () => {
             <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
              <ul className='flex
          flex-col gap-2 text-gray-600'>
-              <li>+1-212-456-7890</li>
+              <li>+91 6260476245</li>
               <li>bookmydoc@gmail.com</li>
              </ul>
         
--- front-end/src/components/navbar.jsx
+++ front-end/src/components/navbar.jsx
@@ -41,17 +41,18 @@ const Navbar = () => {
         {
           token
           ?<div className='flex items-center gap-2 cursor-pointer group relative'>
-            <img className='w-8 rounded-full' src={assets.profile_pic} alt="" />
-            <img className='w-2.5' src={assets.dropdown_icon} alt="" />
-            <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
-              <div className='min-w-48 bg-stone-100 rounded flex fle-col gap-4 p-4'>
-                <p onClick={()=>navigate('My-Profile')} className='hover:text-black cursor-pointer'>My Profile</p>
-                <p onClick={()=>navigate('My-appointment')}  className='hover:text-black cursor-pointer'>My Appointments</p>
-                <p onClick={()=>(setToken())} className='hover:text-black cursor-pointer'>Logout</p> 
-              </div>
+          <img className='w-8 rounded-full' src={assets.profile_pic} alt="" />
+          <img className='w-2.5' src={assets.dropdown_icon} alt="" />
+          <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:flex flex-col'>
+            <div className='min-w-48 bg-stone-100 rounded flex fle-col gap-4 p-4'>
+              <p onClick={()=>navigate('My-Profile')} className='hover:text-black cursor-pointer'>My Profile</p>
+              <p onClick={()=>navigate('My-appointment')}  className='hover:text-black cursor-pointer'>My Appointments</p>
+              <p onClick={()=>(setToken())} className='hover:text-black cursor-pointer'>Logout</p> 
             </div>
           </div>
-          :        <button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
+        </div>
+        :        <button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
+        
         }
 
       </div>
