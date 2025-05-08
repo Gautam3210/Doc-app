0a6c4a7a88785bada41d5bcfb2eaca8956427910
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-05-08 23:28:29 +0530

    add some code

--- front-end/src/context/AppContext.jsx
+++ front-end/src/context/AppContext.jsx
@@ -20,4 +20,6 @@ const AppContextProvider = (props) => {
   );
 };
 
+
+
 export default AppContextProvider;
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -6,6 +6,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import AppContextProvider from './context/AppContext.jsx'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
