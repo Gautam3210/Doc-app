98c63a32a51a02c45b87f9a994650097e725bcf4
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-05-18 22:49:18 +0530

    add some code

--- back-end/controllers/admincontroller.js
+++ back-end/controllers/admincontroller.js
@@ -5,6 +5,7 @@ import doctorModel from "../models/doctorModel.js"
 
 //API for adding doctor
 
+
 const addDoctor = async (req,res)=>{
   try{
     const {name,email,password,speciality,degree,experience,about,fees,address}=req.body
