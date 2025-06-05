94f3b99427152787c4fd61762c1f0fa86352121d
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-06-05 13:12:51 +0530

    add code in front-end

--- front-end/src/assets/assets.js
+++ front-end/src/assets/assets.js
@@ -96,7 +96,8 @@ export const doctors = [
         about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
         fees: 50,
         address: {
-            line1: '17th Cross, Richmond',
+         
+            line1: '52th Cross, Richmond',
             line2: 'Circle, Ring Road, London'
         }
     },
