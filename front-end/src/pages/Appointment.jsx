f222d7869e0cb2fe3d7873104c02aaca6ad46a3e
Gautam Malviya <gmalviya619@gmail.com>
2025-06-28 17:21:58 +0530

    fixed the bug

--- front-end/src/pages/Appointment.jsx
+++ front-end/src/pages/Appointment.jsx
@@ -161,7 +161,7 @@ const Appointment = () => {
               alt=""
             />
           </div>
-          <div className="flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0" style={{background:"#ebddf5ad"}}>
+          <div className="flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-8px] sm:mt-0" style={{background:"#ebddf5ad"}}>
             <p className="flex items-center gap-2 text-2xl font-medium text-gray-900">
               {docInfo.name}
               <img className="w-5" src={assets.verified_icon} alt="" />
