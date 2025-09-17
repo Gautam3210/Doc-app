a93828b19253a501500ce30de91d0bbcaf2feecf
Gautam Malviya <113026077+Gautam3210@users.noreply.github.com>
2025-09-18 01:09:21 +0530

    currency symbol changed

--- front-end/src/context/AppContext.jsx
+++ front-end/src/context/AppContext.jsx
@@ -8,7 +8,7 @@ export const AppContext = createContext();
 
 // Context provider component
 const AppContextProvider = (props) => {
-  const currencySymbol = "$";
+  const currencySymbol = "₹";
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
   const [doctors, setDoctors] = useState([]);
